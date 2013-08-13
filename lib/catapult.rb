5c309a5ac366f08d80fcf99bbc7f68ec82609956
Caleb <cbron@azcentral.com>
2013-08-13 14:34:39 -0700

    added campaign class and method

--- lib/catapult.rb
+++ lib/catapult.rb
@@ -2,6 +2,7 @@ require "net/http"
 require "uri"
 require 'catapult/catapult_methods'
 require 'catapult/subscription'
+require 'catapult/campaign'
 require 'catapult/version'
 
 
---
+++ lib/catapult/campaign.rb
@@ -0,0 +1,18 @@
+module Catapult
+  class Campaign
+    extend CatapultMethods
+
+    def self.list_campaigns(account_list=nil, status_filter=nil, type_filter=nil)
+      req_str = "/api/campaigns.xml?"
+      arr = {}
+      arr[:accounts] = account_list if account_list
+      arr[:status] = status_filter if status_filter
+      arr[:type] = type_filter if type_filter
+      http, request = build_request(req_str + arr.to_query)
+      response = http.request(request)
+      Hash.from_xml(response.body)
+    end
+
+  end
+end
+
